1a3ea7cee5cea6ece28d481d66f829332e6d958d
satyasashi <sashikanthpeddinti@gmail.com>
2021-11-20 16:51:54 +0530

    added a test comment in treetraversal.py

--- Datastructures/BinaryTree/treetraversal.py
+++ Datastructures/BinaryTree/treetraversal.py
@@ -33,6 +33,7 @@ def printPreOrder(root):
 
 if __name__ == "__main__":
     root = Node(11)
+    # test comment
     root.left = Node(6)
     root.right = Node(15)
 
@@ -42,16 +43,15 @@ if __name__ == "__main__":
     root.right.left = Node(13)
     root.right.right = Node(17)
 
-    root.left.left.left  = Node(1)
-    root.left.left.right  = Node(5)
+    root.left.left.left = Node(1)
+    root.left.left.right = Node(5)
 
     # right subtree
     root.right.left.left = Node(12)
     root.right.left.right = Node(14)
 
-    root.right.right  = Node(17)
-    root.right.right.right  = Node(19)
-
+    root.right.right = Node(17)
+    root.right.right.right = Node(19)
 
     print("Inorder traversal of Binary Tree: ")
     printInorder(root)
