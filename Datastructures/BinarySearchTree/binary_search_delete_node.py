82ff8c43108af72c7b973a9e6a12cf5780d4d8ea
satyasashi <sashikanthpeddinti@gmail.com>
2021-11-21 21:20:33 +0530

    delete node binary search tree

--- Datastructures/BinarySearchTree/binary_search_delete_node.py
+++ Datastructures/BinarySearchTree/binary_search_delete_node.py
@@ -1,8 +1,31 @@
 from binaryInsertionOfKey import BinarySearchTree, Node
 
+
 class DeleteNode(BinarySearchTree):
     """Deletes a specified key from the Binary Tree"""
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def deleteNode(self, root, key):
+        """
+        Takes in a Root reference and a Key to delete.
+        Case 1: Node is a Leaf
+        Case 2: Node to be deleted has 1 Children
+        Case 3: Node to be deleted has 2 Children
+
+        Case 1:
+        Traverse the BST till you find the next of the Node is the Key then remove the pointer and del the Key.
+
+        """
+        if root is not None:
+            if root.data < key:
+                # left subtree
+                self.deleteNode(root.left)
+            elif root.data > key:
+                # right subtree
+                self.deleteNode(root.right)
+
 
 # driver code
 if __name__ == "__main__":
